20441ad9aa65cf5700f85bc7e3ca6075c0e1b7c7
Ahmet Can Topçu <ahmetcanntopcu@gmail.com>
2023-05-08 19:07:56 +0300

    first commit

---
+++ AppcentTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppcentTask
+//
+//  Created by Ahmet Can Topcu on 8.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
